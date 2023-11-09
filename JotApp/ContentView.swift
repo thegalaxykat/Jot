29cc882d2873b16a4d41c2b63e138d015d009ce2
thegalaxykat <katcanavan@gmail.com>
2023-11-08 23:06:48 -0500

    Add menu button

--- JotApp/ContentView.swift
+++ JotApp/ContentView.swift
@@ -12,21 +12,39 @@ struct ContentView: View {
     @State var text = loadInitialText()
 
     var body: some View {
-        VStack {
-            Image(systemName: "square.and.pencil")
-                .imageScale(.large)
-                .foregroundColor(.white)
-            Spacer()
-                .frame(height:15)
-            TextEditor(text: $text)
-                .frame(width: 275, height: 300)
-                .font(.system(size: 14))
-                // save text after change
-                .onChange(of:text, debounceTime: .seconds(3)) { newValue in
-                    saveText(data: text)
+        ZStack {
+            // Button(s) at the top
+            HStack() { // should be at top of frame
+                Spacer()
+                    .frame(width:260)
+                Button() {
+                    print("test")
+                } label: {
+                    Image(systemName: "ellipsis")
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 3)
                 }
+            }
+                .frame(height: 355, alignment: .top)
+                .buttonStyle(.borderless)
+            
+            // Main App
+            VStack {
+                Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
+                Spacer()
+                    .frame(height:15)
+                TextEditor(text: $text)
+                    .frame(width: 275, height: 300)
+                    .font(.system(size: 14))
+                    // save text after change (with delay)
+                    .onChange(of:text, debounceTime: .seconds(3)) { newValue in
+                        saveText(data: text)
+                    }
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
